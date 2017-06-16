226f71d186cbb2f557e11e6e1be374e6117776da
silvana.denisse.15@gmail.com <silvanadenisse@gmail.com>
2017-06-16 03:12:19 -0600

    css other

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  Area.create(nombre: 'Oncologia')
+  Area.create(nombre: 'Oncología')
+  
