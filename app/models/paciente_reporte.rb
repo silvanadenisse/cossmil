aa58278b4a42bc947f45196a0d46a7c2644c6899
silvana.denisse.15@gmail.com <silvanadenisse@gmail.com>
2017-06-16 05:02:36 -0600

    form modal

--- app/models/paciente_reporte.rb
+++ app/models/paciente_reporte.rb
@@ -2,6 +2,9 @@ class PacienteReporte
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
   field :diagnostico, type: String
+  field :edad, type: Integer
+  field :tipo_consulta, type: Mongoid::Boolean
+  field :tipo_asegurado, type: Mongoid::Boolean
   belongs_to :paciente
   belongs_to :parte_diario
 end
